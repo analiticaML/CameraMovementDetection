141b1ae2065261fb5746b04df581267ba1c697bb
SeleneSV <ssolanov@unal.edu.co>
2022-09-23 13:45:41 -0500

    humanDetector

---
+++ humanDetector.py
@@ -0,0 +1,45 @@
+import cv2
+import imutils
+import numpy as np
+import argparse
+
+def detect(frame):
+    bounding_box_cordinates, weights =  HOGCV.detectMultiScale(frame, winStride = (4, 4), padding = (8, 8), scale = 1.03)
+    
+    person = 1
+    for x,y,w,h in bounding_box_cordinates:
+        cv2.rectangle(frame, (x,y), (x+w,y+h), (0,255,0), 2)
+        cv2.putText(frame, f'person {person}', (x,y), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0,0,255), 1)
+        person += 1
+    
+    cv2.putText(frame, 'Status : Detecting ', (40,40), cv2.FONT_HERSHEY_DUPLEX, 0.8, (255,0,0), 2)
+    cv2.putText(frame, f'Total Persons : {person-1}', (40,70), cv2.FONT_HERSHEY_DUPLEX, 0.8, (255,0,0), 2)
+    cv2.imshow('output', frame)
+
+    return frame
+
+def humanDetector():
+    image_path = "C:/Users/user/Pictures/profe.jpg"
+
+    print('[INFO] Opening Image from path.')
+    detectByPathImage(image_path)
+
+
+def detectByPathImage(path):
+    image = cv2.imread(path)
+
+    image = imutils.resize(image, width = min(800, image.shape[1])) 
+
+    result_image = detect(image)
+
+    # if output_path is not None:
+    #     cv2.imwrite(output_path, result_image)
+
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+if __name__ == "__main__":
+    HOGCV = cv2.HOGDescriptor()
+    HOGCV.setSVMDetector(cv2.HOGDescriptor_getDefaultPeopleDetector())
+
+    humanDetector()
